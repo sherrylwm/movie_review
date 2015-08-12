91580bf602bb0ba7318ffe640e12a06dc52a0d21
sherrylwm <sherrylim.saw@gmail.com>
2015-08-12 07:42:17 +0000

    Added imagemagick and posters

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+    
+    has_attached_file :image, styles: { medium: "400x600#" }
+    validates_attachment_content_type :image, content_type: [/\Aimage\/.*\Z/, "file/jfif"]
+
 end
